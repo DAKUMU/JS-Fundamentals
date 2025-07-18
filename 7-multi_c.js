ae014c1483c1786b1f36d81a93aef4655aa11df7
David Akumu <david.akumu@gmail.com>
2025-07-18 05:13:59 +0300

    Task 7: Print 'C is fun' x times or error message

---
+++ 7-multi_c.js
@@ -0,0 +1,12 @@
+const arg = process.argv[2];
+const x = parseInt(arg, 10);
+
+if (!arg || isNaN(x) || x <= 0) {
+  console.log('Missing number of occurrences');
+} else {
+  let i = 0;
+  while (i < x) {
+    console.log('C is fun');
+    i++;
+  }
+}
