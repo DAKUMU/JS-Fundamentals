83ee2e644145c98e5af665d1844ed2a49c7f119e
David Akumu <david.akumu@gmail.com>
2025-07-18 05:15:18 +0300

    Fix Task 7: No output for zero or negative numbers

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,12 +1,13 @@
 const arg = process.argv[2];
 const x = parseInt(arg, 10);
 
-if (!arg || isNaN(x) || x <= 0) {
+if (!arg || isNaN(x)) {
   console.log('Missing number of occurrences');
-} else {
+} else if (x > 0) {
   let i = 0;
   while (i < x) {
     console.log('C is fun');
     i++;
   }
 }
+// If x is 0 or negative, print nothing
